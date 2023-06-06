0de42591dd74269b289db774de762a00a3f64351
اَمیرمَمّد <amirmmd.ltf@stu.nit.ac.ir>
2023-06-07 02:06:46 +0330

    using state for making code more dynamic and combination with array.map()

--- react-native/src/app.js
+++ react-native/src/app.js
@@ -45,17 +45,39 @@ import { render } from "react-dom";
 
 //----------------- to this code in 5th step with converting function to class  -> ----------------
 
+// class AppComponent extends React.Component {
+//   render() {
+//     return (
+//       <div className="detail" id="about">
+//         <h1>we use component method</h1>
+//         <Product name="phone" price="699" />
+//         <Product name="iPod" price="799" />
+//         {/* for children we use this method ↓ */}
+//         <Product name="socks" price="25">
+//           <p>discount is 15%</p>
+//         </Product>
+//       </div>
+//     );
+//   }
+// }
+
+// let's check this code with .map ->
+
 class AppComponent extends React.Component {
+  state = {
+    products: [
+      { name: "iphone", price: 699 },
+      { name: "iPad", price: 799 },
+      { name: "socks", price: 25 },
+    ],
+  };
   render() {
     return (
       <div className="detail" id="about">
-        <h1>we use component method</h1>
-        <Product name="phone" price="699" />
-        <Product name="iPod" price="799" />
-        {/* for children we use this method ↓ */}
-        <Product name="socks" price="25">
-          <p>discount is 15%</p>
-        </Product>
+        <h1>we use component method v6</h1>
+        {this.state.products.map((product) => {
+          return <Product name={product.name} price={product.price} />;
+        })}
       </div>
     );
   }
@@ -64,3 +86,6 @@ class AppComponent extends React.Component {
 export default AppComponent;
 
 // 5. let's convert function to class -> { line 47 }
+
+// 6. using state for make it more dynamic :
+//      - adding state to AppComponent -> { line 65 }
