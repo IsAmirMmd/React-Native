57f60e9a9129c754add78aa0251e5b9fb9528f6c
اَمیرمَمّد <amirmmd.ltf@stu.nit.ac.ir>
2023-06-07 01:08:29 +0330

    combine components in app.js and using it in smart way!

--- react-native/src/app.js
+++ react-native/src/app.js
@@ -1,10 +1,27 @@
 import React from "react";
+// we import Product.js in 3rd step :
+import Product from "./components/Products/Product";
+
+//----------------- we change this code from 2nd step to -> ------------------
+
+// const AppComponent = () => {
+//   return (
+//     <div className="detail" id="about">
+//       <h1>we use component method</h1>
+//       <p>test</p>
+//     </div>
+//   );
+// };
+
+//----------------- to this code in 3rd step -> ------------------------------
 
 const AppComponent = () => {
   return (
     <div className="detail" id="about">
       <h1>we use component method</h1>
-      <p>test</p>
+      <Product />
+      <Product />
+      <Product />
     </div>
   );
 };
---
+++ react-native/src/components/Products/Product.js
@@ -0,0 +1,13 @@
+// 3. using component to avoid from complex in code
+
+// we use an shortcut snippet "sfc"
+
+const Product = () => {
+  return <p>this is test product</p>;
+};
+
+export default Product;
+
+// for rendering it :
+// 1. we have to import it in app.js
+// 2. and then use <export_name /> in app.js
