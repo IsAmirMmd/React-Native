3be7d1a84e7d208ae05529582680154f1dff1f2a
اَمیرمَمّد <amirmmd.ltf@stu.nit.ac.ir>
2023-07-06 17:59:58 +0330

    useContext and useReducer combined and the context folder updated

--- react-native/src/components/Context/CounterOne.jsx
+++ react-native/src/components/Context/CounterOne.jsx
@@ -1,15 +1,19 @@
 import React from "react";
-import { CounterContext } from "./CounterProvider";
-import { CounterContextDispatcher } from "./CounterProvider";
+import { CounterContext, useCountAction } from "./CounterProvider";
 import { useContext } from "react";
 
 const CounterOne = () => {
   const count = useContext(CounterContext);
-  const setCount = useContext(CounterContextDispatcher);
+  const dispatch = useCountAction();
   return (
     <div>
       <p>{count}</p>
-      <button onClick={() => setCount(count + 1)}>add one</button>
+      <button onClick={() => dispatch({ type: "add", value: 1 })}>
+        add one
+      </button>
+      <button onClick={() => dispatch({ type: "decrease", value: 1 })}>
+        decrease one
+      </button>
     </div>
   );
 };
--- react-native/src/components/Context/CounterProvider.jsx
+++ react-native/src/components/Context/CounterProvider.jsx
@@ -1,13 +1,27 @@
-import React, { useState } from "react";
+import React, { useContext, useReducer, useState } from "react";
 
 export const CounterContext = React.createContext(); // state
 export const CounterContextDispatcher = React.createContext(); // set state
 
+const initialState = 0;
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "add":
+      return state + action.value;
+    case "decrease":
+      return state - action.value;
+    default:
+      return state;
+  }
+};
+
 const CounterProvider = ({ children }) => {
-  const [count, setCount] = useState(0);
+  const [count, dispatch] = useReducer(reducer, initialState);
+
   return (
     <CounterContext.Provider value={count}>
-      <CounterContextDispatcher.Provider value={setCount}>
+      <CounterContextDispatcher.Provider value={dispatch}>
         {children}
       </CounterContextDispatcher.Provider>
     </CounterContext.Provider>
@@ -20,3 +34,7 @@ export default CounterProvider;
 // 1. for make the app more effecient you can use function and export them
 // 2. ex=> export const useCount  = () => useContext(CounterContext)
 // 3. and then import it in another files
+
+export const useCountAction = () => {
+  return useContext(CounterContextDispatcher);
+};
