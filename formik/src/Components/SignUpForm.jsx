a424bb8e44260fbf8a7f51b7b87a2d1481ec09ad
اَمیرمَمّد <amirmmd.ltf@stu.nit.ac.ir>
2023-07-18 12:46:34 +0330

    condition added for password inputs

--- formik/src/Components/SignUpForm.jsx
+++ formik/src/Components/SignUpForm.jsx
@@ -6,6 +6,7 @@ const initialValues = {
   name: "",
   email: "",
   password: "",
+  RePassword: "",
 };
 
 // 2. submit
@@ -28,7 +29,16 @@ const validate = (values) => {
 const validationSchema = Yup.object({
   name: Yup.string().required("Name is Required"),
   email: Yup.string().email("email format").required("email is Required"),
-  password: Yup.string().required("password is Required"),
+  password: Yup.string()
+    .required("password is Required")
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})/,
+      "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character"
+    ),
+  RePassword: Yup.string().oneOf(
+    [Yup.ref("password"), null],
+    "Passwords must match"
+  ),
 });
 
 const SignUpForm = () => {
@@ -43,26 +53,14 @@ const SignUpForm = () => {
       <form onSubmit={formik.handleSubmit}>
         <div className="formControl">
           <label>name</label>
-          <input
-            name="name"
-            value={formik.values.name}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            type="text"
-          />
+          <input name="name" {...formik.getFieldProps("name")} type="text" />
           {formik.errors.name && formik.touched.name && (
             <p className="error-onForm">{formik.errors.name}</p>
           )}
         </div>
         <div className="formControl">
           <label>email</label>
-          <input
-            name="email"
-            value={formik.values.email}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            type="text"
-          />
+          <input name="email" {...formik.getFieldProps("email")} type="text" />
           {formik.errors.email && formik.touched.email && (
             <p className="error-onForm">{formik.errors.email}</p>
           )}
@@ -71,13 +69,18 @@ const SignUpForm = () => {
           <label>password</label>
           <input
             name="password"
-            value={formik.values.password}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
+            {...formik.getFieldProps("password")}
             type="text"
           />
           {formik.errors.password && formik.touched.password && (
             <p className="error-onForm">{formik.errors.password}</p>
+          )}
+        </div>
+        <div className="formControl">
+          <label>enter password again</label>
+          <input name="P" {...formik.getFieldProps("RePassword")} type="text" />
+          {formik.errors.RePassword && formik.touched.RePassword && (
+            <p className="error-onForm">{formik.errors.RePassword}</p>
           )}
         </div>
         <button type="submit">submit</button>
